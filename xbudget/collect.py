698bbed0d0e036552584f5d29b825601ec9fca02
Henri Drake <henrifdrake@gmail.com>
2023-07-17 14:10:35 -0400

    WIP: Fill dataset with sum/product terms
    Right now, this method only does a shallow search of the budget. We really want to be able to fill every missing term in the budget!

--- xbudget/collect.py
+++ xbudget/collect.py
@@ -1,42 +1,60 @@
 from operator import mul
 from functools import reduce
 
-def collect_terms(ds, budget_dict, mode="shallow"):
-    return flatten_lol(_budget_search(ds, budget_dict, mode=mode))
-
-def _budget_search(ds, d, mode="shallow"):
+def collect_budgets(ds, budgets_dict, mode="shallow"):
+    for eq, v in budgets_dict.items():
+        for part in ["rhs", "lhs", "surface_advective_ocean_flux"]:
+            if part in v:
+                budget_search(ds, v[part], f"{eq}_{part}", mode=mode)
+                
+def budget_search(ds, d, namepath, mode="shallow"):
     if type(d) is float:
-            return d
+        return d
+    
     if mode=="shallow":
-        if "total" in d:
-            if d["total"] is not None:
-                return([d["total"]])
+        if "var" in d:
+            if d["var"] is not None:
+                return([d["var"]])
         if "product" in d:
-            l = [_budget_search(ds, v, mode=mode) for k,v in d["product"].items()]
-            product_str = "product_"+"_x_".join([str(e) for e in l])
-            ds[f"{product_str}"] = reduce(mul, [v if type(v) is float else ds[v] for v in l], 1)
-            return([product_str])
-        elif "sum" in d:
-            return([_budget_search(ds, v, mode=mode) for k,v in d["sum"].items()])
-
+            vname = f"{namepath}_product"
+            d["product"]["var"] = vname
+            if d["var"] is None:
+                d["var"] = vname
+            l = [budget_search(ds, v, f"{vname}", mode=mode) for k,v in d["product"].items() if k !="var"]
+            ds[vname] = reduce(mul, [e if type(e) is float else ds[e] for e in l if e !="var"], 1)
+            return([vname])
+        if "sum" in d:
+            vname = f"{namepath}_sum"
+            d["sum"]["var"] = vname
+            if d["var"] is None:
+                d["var"] = vname
+            sum_list = flatten_lol([budget_search(ds, v, f"{namepath}_{k}", mode=mode) for k,v in d["sum"].items() if k !="var"])
+            ds[vname] = sum([ds[v] for v in sum_list])
+            return(sum_list)
         
     elif mode=="deep":
         if "sum" in d:
-            return([_budget_search(ds, v, mode=mode) for k,v in d["sum"].items()])
-        elif "product" in d:
-            l = [_budget_search(ds, v, mode=mode) for k,v in d["product"].items()]
-            product_str = "product_"+"_x_".join([str(e) for e in l])
-            ds[f"{product_str}"] = reduce(mul, [v if type(v) is float else ds[v] for v in l], 1)
-            return([product_str])
-        elif "total" in d:
-            if d["total"] is not None:
-                return([d["total"]])
-            
+            vname = f"{namepath}_sum"
+            d["sum"]["var"] = vname
+            if d["var"] is None:
+                d["var"] = vname
+            sum_list = flatten_lol([budget_search(ds, v, f"{namepath}_{k}", mode=mode) for k,v in d["sum"].items() if k !="var"])
+            ds[vname] = sum([ds[v] for v in sum_list])
+            return(sum_list)
+        if "product" in d:
+            vname = f"{namepath}_product"
+            d["product"]["var"] = vname
+            if d["var"] is None:
+                d["var"] = vname
+            l = [budget_search(ds, v, f"{vname}", mode=mode) for k,v in d["product"].items() if k !="var"]
+            ds[vname] = reduce(mul, [e if type(e) is float else ds[e] for e in l if e !="var"], 1)
+            return([vname])
+        
     if type(d) is str:
         return d
     else:
         raise ValueError("Can not find term.")
-        
+
 def flatten(container):
     for i in container:
         if isinstance(i, (list,tuple)):
--- xbudget/presets.py
+++ xbudget/presets.py
@@ -1,9 +1,10 @@
 import yaml
+from os import path
 
 def load_preset_budget(model="MOM6"):
-    with open(f"../conventions/{model}.yaml", "r") as stream:
+    with open(f"{path.dirname(__file__)}/conventions/{model}.yaml", "r") as stream:
         try:
-            budgets_dict = yaml.safe_load(stream)
+            budgets_dict = yaml.safe_f"{namepath}_product_{k}",load(stream)
         except yaml.YAMLError as exc:
             print(exc)
     return budgets_dict
