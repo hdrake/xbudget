d970f3fecf835d7dc2c79422c0870e34f8596f0e
Henri Drake <henrifdrake@gmail.com>
2023-07-20 13:19:02 -0400

    Fill gaps in budget dict with derived terms
    Major changes:
    - The xarray dataset containing diagnostics is not passed to the xbudget
    functions so that derived terms can be (lazily) added to the dataset. The names of derived
    variables follow from the nested dictionary structure and are unambiguous.
    - dictionary structure has been modified so that the names of the new derived variables are also
    added to the dictionary. This facilitates evaluation of the reconstructed terms as it is now obvious
    if there are existing diagnostics that they should be equivalent to.
    - the collect_budgets function fills the budget with derived terms and the `aggregate` function
    is used to collect just some high-level terms
    
    To do:
    - Functions for more flexible grouping/aggregating/decomposing of budget terms. For example, what if someone wants a
      high-level budget for most terms but wants to really break down the surface fluxes as much as possible? They currently
      can do this manually by following the budget structure, but it would be nice to have helper functions to make it easier.
    - Support derived variables that are finite-differences of fluxes, to be compared against the flux-divergence diagnostics.
    - Somehow protect against users combining inconsistent terms (e.g. on different grids).

--- xbudget/collect.py
+++ xbudget/collect.py
@@ -1,59 +1,77 @@
 from operator import mul
 from functools import reduce
+import copy
 
-def collect_budgets(ds, budgets_dict, mode="shallow"):
+def aggregate(budgets):
+    new_budgets = copy.deepcopy(budgets)
+    for tr,budget in budgets.items():
+        for part,terms in budget.items():
+            if part in ["lhs", "rhs"]:
+                new_budgets[tr][part] = {}
+                for k,v in budget[part]["sum"].items():
+                    if k=="var": continue
+                    if "var" in v:
+                        new_budgets[tr][part][k] = v['var']
+                    else:
+                        new_budgets[tr][part][k] = v
+            elif part in ["surface_advective_ocean_flux"]:
+                new_budgets[tr][part] = {part: budget[part]["var"]}
+                
+    return new_budgets
+
+def collect_budgets(ds, budgets_dict):
     for eq, v in budgets_dict.items():
         for part in ["rhs", "lhs", "surface_advective_ocean_flux"]:
             if part in v:
-                budget_search(ds, v[part], f"{eq}_{part}", mode=mode)
+                budget_fill(ds, v[part], f"{eq}_{part}", mode="sum_first")
+                budget_fill(ds, v[part], f"{eq}_{part}", mode="product_first")
                 
-def budget_search(ds, d, namepath, mode="shallow"):
-    if type(d) is float:
-        return d
-    
-    if mode=="shallow":
-        if "var" in d:
-            if d["var"] is not None:
-                return([d["var"]])
-        if "product" in d:
-            vname = f"{namepath}_product"
-            d["product"]["var"] = vname
-            if d["var"] is None:
-                d["var"] = vname
-            l = [budget_search(ds, v, f"{vname}", mode=mode) for k,v in d["product"].items() if k !="var"]
-            ds[vname] = reduce(mul, [e if type(e) is float else ds[e] for e in l if e !="var"], 1)
-            return([vname])
-        if "sum" in d:
-            vname = f"{namepath}_sum"
-            d["sum"]["var"] = vname
-            if d["var"] is None:
-                d["var"] = vname
-            sum_list = flatten_lol([budget_search(ds, v, f"{namepath}_{k}", mode=mode) for k,v in d["sum"].items() if k !="var"])
-            ds[vname] = sum([ds[v] for v in sum_list])
-            return(sum_list)
-        
-    elif mode=="deep":
-        if "sum" in d:
-            vname = f"{namepath}_sum"
-            d["sum"]["var"] = vname
-            if d["var"] is None:
-                d["var"] = vname
-            sum_list = flatten_lol([budget_search(ds, v, f"{namepath}_{k}", mode=mode) for k,v in d["sum"].items() if k !="var"])
-            ds[vname] = sum([ds[v] for v in sum_list])
-            return(sum_list)
-        if "product" in d:
-            vname = f"{namepath}_product"
-            d["product"]["var"] = vname
-            if d["var"] is None:
-                d["var"] = vname
-            l = [budget_search(ds, v, f"{vname}", mode=mode) for k,v in d["product"].items() if k !="var"]
-            ds[vname] = reduce(mul, [e if type(e) is float else ds[e] for e in l if e !="var"], 1)
-            return([vname])
-        
-    if type(d) is str:
-        return d
+def budget_fill(ds, budget, namepath, mode="sum_first"):
+    if type(budget) is float:
+        return budget
+    elif type(budget) is str:
+        return ds[budget]
+    elif type(budget) is dict:
+        if mode=="sum_first":
+            if "sum" in budget:
+                vname = f"{namepath}_sum"
+                sum_list = [budget_fill(ds, v, f"{namepath}_{k}", mode=mode) for k,v in budget["sum"].items() if k!="var"]
+                budget["sum"]["var"] = vname
+                if budget["var"] is None:
+                    budget["var"] = vname
+                ds[vname] = sum([v for v in sum_list])
+                return ds[vname]
+            elif "product" in budget:
+                vname = f"{namepath}_product"
+                mul_list = [budget_fill(ds, v, f"{namepath}", mode=mode) for k,v in budget["product"].items() if k!="var"]
+                budget["product"]["var"] = vname
+                if budget["var"] is None:
+                    budget["var"] = vname
+                ds[vname] = reduce(mul, [e for e in mul_list], 1)
+                return ds[vname]
+            elif "var" in budget:
+                return ds[budget["var"]]
+        elif mode=="product_first":
+            if "product" in budget:
+                vname = f"{namepath}_product"
+                mul_list = [budget_fill(ds, v, f"{namepath}", mode=mode) for k,v in budget["product"].items() if k!="var"]
+                budget["product"]["var"] = vname
+                if budget["var"] is None:
+                    budget["var"] = vname
+                ds[vname] = reduce(mul, [e for e in mul_list], 1)
+                return ds[vname]
+            elif "sum" in budget:
+                vname = f"{namepath}_sum"
+                sum_list = [budget_fill(ds, v, f"{namepath}_{k}", mode=mode) for k,v in budget["sum"].items() if k!="var"]
+                budget["sum"]["var"] = vname
+                if budget["var"] is None:
+                    budget["var"] = vname
+                ds[vname] = sum([v for v in sum_list])
+                return ds[vname]
+            elif "var" in budget:
+                return ds[budget["var"]]
     else:
-        raise ValueError("Can not find term.")
+        raise ValueError("Broken.")
 
 def flatten(container):
     for i in container:
--- xbudget/presets.py
+++ xbudget/presets.py
@@ -4,7 +4,7 @@ from os import path
 def load_preset_budget(model="MOM6"):
     with open(f"{path.dirname(__file__)}/conventions/{model}.yaml", "r") as stream:
         try:
-            budgets_dict = yaml.safe_f"{namepath}_product_{k}",load(stream)
+            budgets_dict = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
     return budgets_dict
