b69b2f10ba3b7704bdc1db2d5adf7a2819b8cba2
Henri Drake <henrifdrake@ch3212.ps.uci.edu>
2023-09-07 14:33:58 -0700

    Remove conventions import

--- src/xbudget/__init__.py
+++ src/xbudget/__init__.py
@@ -1,5 +1,4 @@
 """ xbudget: xarray and xgcm-based functions for evaluating finite-volume budgets"""
 from .presets import *
 from .collect import *
-import conventions
 from .version import __version__
